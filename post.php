5b9dec0eab38c6128786b92d7284c1caa3be2ae5
安和 <42889600+AHQAQ@users.noreply.github.com>
2019-08-11 18:58:58 +0800

    文章样式小改 去除冗余代码

--- post.php
+++ post.php
@@ -9,25 +9,13 @@ $this->need('header.php');
 <link rel="stylesheet" href="<?php $this->options->themeUrl('/css/post.css'); ?>"> 
   <div class="main">
   <div class="article-list">
- 
-       <div class="content">
-         <?php  $this->need('side.php'); ?>
-      <?php while($this->next()): ?>
-    
-     
-	
-	  <article itemscope="" itemtype="http://schema.org/BlogPosting">
-
-      <h2 style="
+<div class="content">
+<?php  $this->need('side.php'); ?>
+<?php while($this->next()): ?>
+<h2 style="
     margin-bottom: 0px;
 "><?php $this->title() ?></h2>
-           <p itemprop="keywords" style="padding-top:10px;margin-top: 0px;""><?php _e('标签: '); ?><?php $this->tags(', ', true, '无标签'); ?></p>
+           <p style="padding-top:10px;margin-top: 0px;"><?php _e('标签: '); ?><?php $this->tags(', ', true, '无标签'); ?></p>
 	 <?php $this->content('Continue Reading...'); ?>
-
-
-	    
-
-
+<hr>
 <?php endwhile; ?>
- 
-  </article><!-- /.blog-post -->
