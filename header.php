49fb25439f7bf15facf11e529fd327abaeb5bfbb
安和 <42889600+AHQAQ@users.noreply.github.com>
2019-08-11 19:24:19 +0800

    修复自适应失效
    修复

--- header.php
+++ header.php
@@ -1,7 +1,14 @@
 <?php
+
 if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
-<link rel="stylesheet" href="<?php $this->options->themeUrl('/css/jianhe.css'); ?>"> 
+<html lang="zh-CN">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+<link rel="stylesheet" href="<?php $this->options->themeUrl('css/jianhe.css'); ?>"> 
  <?php $this->header(); ?>
+</head>
 <title> <?php $this->options->title(); ?><?php $this->archiveTitle(); ?> </title>
 
 
