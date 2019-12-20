7c8e7484ff3b765ff6d54f8fd80346307f10eaae
安和 <42889600+AHQAQ@users.noreply.github.com>
2019-12-20 19:20:39 +0800

    精简代码 样式小改

--- post.php
+++ post.php
@@ -7,9 +7,7 @@ $this->need('header.php');?>
 <div class="article-list">
 <div class="content">
 <?php  $this->need('side.php'); ?>
-<?php while($this->next()): ?>
 <h2 style="margin-bottom: 0px;"><?php $this->title() ?></h2>
 <p style="padding-top:10px;margin-top: 0px;"><?php _e('标签: '); ?><?php $this->tags(', ', true, '无标签'); ?></p>
-<?php $this->content('Continue Reading...'); ?>
-<hr>
+<?php $this->content(); ?>
 <?php endwhile; ?>
