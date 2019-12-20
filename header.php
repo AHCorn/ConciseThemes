1617d92785d1eaa0061b93fb1790c007c746df6f
安和 <42889600+AHQAQ@users.noreply.github.com>
2019-12-20 19:17:58 +0800

    Update header.php

--- header.php
+++ header.php
@@ -1,14 +1,10 @@
 <?php
-
 if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
-<html lang="zh-CN">
+<html>
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
 <link rel="stylesheet" href="<?php $this->options->themeUrl('css/jianhe.css'); ?>"> 
  <?php $this->header(); ?>
 </head>
-<title> <?php $this->options->title(); ?><?php $this->archiveTitle(); ?> </title>
+  <title> <?php $this->options->title(); ?><?php $this->archiveTitle(); ?> </title></head>
 
 
