c5f0b94416fb36f4a0b8fd1ef1f4a7f2a9184901
安和 <42889600+AHQAQ@users.noreply.github.com>
2019-12-20 19:19:09 +0800

    精简代码

--- post.php
+++ post.php
@@ -1,21 +1,15 @@
 <?php
 
 if (!defined('__TYPECHO_ROOT_DIR__')) exit;
-$this->need('header.php');
-?>
-<html>
-
-<body>
+$this->need('header.php');?>
 <link rel="stylesheet" href="<?php $this->options->themeUrl('/css/post.css'); ?>"> 
-  <div class="main">
-  <div class="article-list">
+<div class="main">
+<div class="article-list">
 <div class="content">
 <?php  $this->need('side.php'); ?>
 <?php while($this->next()): ?>
-<h2 style="
-    margin-bottom: 0px;
-"><?php $this->title() ?></h2>
-           <p style="padding-top:10px;margin-top: 0px;"><?php _e('标签: '); ?><?php $this->tags(', ', true, '无标签'); ?></p>
-	 <?php $this->content('Continue Reading...'); ?>
+<h2 style="margin-bottom: 0px;"><?php $this->title() ?></h2>
+<p style="padding-top:10px;margin-top: 0px;"><?php _e('标签: '); ?><?php $this->tags(', ', true, '无标签'); ?></p>
+<?php $this->content('Continue Reading...'); ?>
 <hr>
 <?php endwhile; ?>
